76b156877b1613dd5734409dcccc3229ef273e47
Ilya Ismagilov <ismagilov@gmail.com>
2017-03-21 10:44:32 +0700

    CountSemiprimes skeleton

---
+++ src/CountSemiprimes.java
@@ -0,0 +1,8 @@
+/**
+* https://codility.com/programmers/lessons/11-sieve_of_eratosthenes/count_semiprimes/
+*/
+class CountSemiprimes {
+    public int[] solution(int N, int[] P, int[] Q) {
+        
+    }
+}
