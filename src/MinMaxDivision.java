d929559d293783a63b2b0278b65a59c343567fb8
Ilya Ismagilov <ismagilov@gmail.com>
2017-03-30 10:56:34 +0700

    Score 66/83 = 75%

--- src/MinMaxDivision.java
+++ src/MinMaxDivision.java
@@ -3,6 +3,44 @@
 */
 class Solution {
     public int solution(int K, int M, int[] A) {
-
+        int minSum = A[0];
+        int maxSum = 0;
+        
+        for (int i = 0; i < A.length; i++) {
+            maxSum += A[i];
+            minSum = Math.min(minSum, A[i]);
+        }
+        
+        int beg = minSum;
+        int end = maxSum;
+        int result = 0;
+        while (beg <= end) {
+            int mid = (beg + end) / 2;
+            
+            if (blocksNumber(A, mid) <= K) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                beg = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    private int blocksNumber(int[] A, int maxSum) {
+        int blocks = 0;
+        int blockSum = 0;
+        
+        for (int i = 0; i < A.length; i++) {
+            if (blockSum + A[i] > maxSum) {
+                blocks++;
+                blockSum = A[i];
+            } else {
+                blockSum += A[i];
+            }
+        }
+        
+        return blocks + 1;
     }
 }
