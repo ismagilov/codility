7382ee61117e6bd32d1b082ac89f22c8d113553d
Ilya Ismagilov <ismagilov@gmail.com>
2017-04-03 16:18:12 +0700

    Solved 100%

--- src/CountDistinctSlices.java
+++ src/CountDistinctSlices.java
@@ -4,29 +4,27 @@ import java.util.*;
 * https://codility.com/programmers/lessons/15-caterpillar_method/count_distinct_slices/
 */
 class Solution {
-    
     public int solution(int M, int[] A) {
-        int[] counters = new int[M + 1];
         int result = 0;
+        
+        int[] counters = new int[M + 1];
         
         int b = 0;
         int e = 0;
-        while (e < A.length) {            
-            while (e < A.length && counters[A[e]] < 1) {
-                counters[A[e]]++;
+        while(b < A.length) {            
+            while(e < A.length && counters[A[e]] == 0) {
+                counters[A[e]] = 1;
                 e++;
-            } 
+            }
             
-            result += sum(e - b);            
-            b = e;
+            result += e - b;
+            if (result > 1000000000)
+                return 1000000000;
             
-            Arrays.fill(counters, 0);
-        }   
+            counters[A[b]] = 0;
+            b++;
+        }
         
         return result;
-    }
-    
-    private int sum(int n) {
-        return (n * n  + n) / 2;
     }
 }
