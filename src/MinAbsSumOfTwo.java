3a667617929efe2b94f7c92f0338f467ca348b40
Ilya Ismagilov <ismagilov@gmail.com>
2017-04-03 17:28:43 +0700

    Result 50/100 = 81%, failed corner cases

---
+++ src/MinAbsSumOfTwo.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/**
+* https://codility.com/programmers/lessons/15-caterpillar_method/min_abs_sum_of_two/
+*/
+class Solution {
+    public int solution(int[] A) {
+        int b = 0;
+        int e = A.length - 1;
+        
+        int min = Math.abs(A[e] + A[b]);
+        
+        Arrays.sort(A);
+        
+        while (b <= e && b < A.length - 1) {
+            while (e >= b) {
+                int curMin = Math.abs(A[b] + A[e]);   
+                
+                min = Math.min(min, curMin);
+                
+                if (e - 1 < 0 || Math.abs(A[b] + A[e - 1]) > curMin)
+                    break;
+                else
+                    e--;
+            }
+            
+            b++;
+        }
+        
+        return min;
+    }
+}
+
