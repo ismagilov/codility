3f640253b24d8d5b91ce13d6975b57aa36319fc6
Ilya Ismagilov <ismagilov@gmail.com>
2017-04-06 17:10:13 +0700

    Result 20/33 = 25%

---
+++ src/NumberSolitaire.java
@@ -0,0 +1,19 @@
+/**
+* https://codility.com/programmers/lessons/17-dynamic_programming/number_solitaire/
+*/
+class Solution {
+    public int solution(int[] A) {
+        int[] max = new int[A.length];
+        
+        max[0] = A[0];
+        max[1] = A[0] + A[1];
+        for (int i = 2; i < max.length; i++) {
+            for (int d = 1; d <= 6; d++) {
+                if (i - d >= 0)
+                    max[i] = Math.max(max[i], A[i] + max[i - d]);
+            }
+        }
+        
+        return max[A.length - 1];
+    }
+}
